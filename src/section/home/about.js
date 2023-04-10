47ee483cdc5000b0c3f732b7aa0227217b73b395
Rakib544 <md.rakib10122003@gmail.com>
2023-04-10 20:34:25 +0600

    some chnages done

--- src/section/home/about.js
+++ src/section/home/about.js
@@ -1,67 +1,104 @@
-import TopTitle from '../../components/TopTitle';
+import TopTitle from "../../components/TopTitle";
 
 export default function Service() {
-    let heading =[
-        {
-            titleData:'About.',
-            subTitle:'WE ARE MORE THAN DIGITAL AGENCY',
-        }
-    ];
+  let heading = [
+    {
+      titleData: "About.",
+      subTitle: "WE ARE MORE THAN DIGITAL AGENCY",
+    },
+  ];
 
-    let items =[
-        {
-            title:"Who we are",
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
-        },
-        {
-            title:"Our philosophy",
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
-        },
-        {
-            title:"How we work",
-            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
-        },
-    ]
+  let items = [
+    {
+      title: "Who we are",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+    },
+    {
+      title: "Our philosophy",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+    },
+    {
+      title: "How we work",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+    },
+  ];
 
-    return(
-        <>
-        <section className="service-section mt-20 md:mt-40 md:px-20 px-12 text-white">
-            <div className='my-20'>
-                {heading.map((headings) =>
-                    <TopTitle headings={headings} />
-                )}
+  return (
+    <>
+      <section className="service-section mt-20 md:mt-40 md:px-20 px-12 text-white">
+        <div className="my-20">
+          {heading.map((headings) => (
+            <TopTitle headings={headings} />
+          ))}
+        </div>
+        {/* <div class="grid grid-cols-2 gap-4 h-96 bg-red-900">
+          <div className="relative h-full col-span-2 lg:col-span-1">
+            <img
+              src="https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o"
+              className="absolute"
+              alt="about1"
+            />
+            <div className="content absolute pl-12 pt-72">
+              <h3 className="font-bold text-2xl">Team Work</h3>
+              <p>Committed and creative</p>
             </div>
-            <div class="grid grid-cols-2 gap-4 h-96 bg-red-900">
-                <div className='relative h-full col-span-2 lg:col-span-1'>
-                    <img src='https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o' className='absolute' alt='about1'/>
-                    <div className='content absolute pl-12 pt-72'>
-                        <h3 className='font-bold text-2xl'>Team Work</h3>
-                        <p>Committed and creative</p>
-                    </div>
-                </div>
-                {/* <div className=' col-span-2 lg:col-span-1'>
-                    <div className='flex flex-col'>
-                        <div className='bg-red-900 w-full'>
-                        <img src='https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o' className='h-auto w-full' alt='about1'/>
-                        </div>
-                        <div className='bg-red-900 '>
-                        <img src='https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o' className='h-auto w-full' alt='about1'/>
-
-                        </div>
-                    </div>
-                </div> */}
+          </div>
+          <div className=" col-span-2 lg:col-span-1">
+            <div className="flex flex-col">
+              <div className="bg-red-900 w-full">
+                <img
+                  src="https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o"
+                  className="h-auto w-full"
+                  alt="about1"
+                />
+              </div>
+              <div className="bg-red-900 ">
+                <img
+                  src="https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o"
+                  className="h-auto w-full"
+                  alt="about1"
+                />
+              </div>
             </div>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-4 pt-12 md:mb-0 w-full bg-slate-900 text-left">
-                {
-                    items.map((item) =>
-                        <div className='bg-[#0B0C10] p-12 md:border-r-4 border-[#66FCF1]'>
-                            <h3 className='text-bold text-2xl pb-4'>{item.title}</h3>
-                            <p>{item.description}</p>
-                        </div>
-                    )
-                }
+          </div>
+        </div> */}
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-12 lg:col-span-6">
+            <img
+              src="https://templatekit.jegtheme.com/pirus/wp-content/uploads/sites/43/2021/01/people-working-in-office-e1613655962551.jpg"
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div className="col-span-12 lg:col-span-6 flex flex-col gap-4">
+            <div className="">
+              <img
+                src="https://templatekit.jegtheme.com/pirus/wp-content/uploads/sites/43/2021/01/people-working-in-office-1.jpg"
+                // className="h-auto w-full"
+                alt="about1"
+              />
             </div>
-        </section>
-        </>
-    )
+            <div className="">
+              <img
+                src="https://templatekit.jegtheme.com/pirus/wp-content/uploads/sites/43/2021/01/office-e1613655942670.jpg"
+                // className="h-auto w-full"
+                alt="about1"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 pt-12 md:mb-0 w-full bg-slate-900 text-left">
+          {items.map((item) => (
+            <div className="bg-[#0B0C10] p-12 md:border-r-4 border-[#66FCF1]">
+              <h3 className="text-bold text-2xl pb-4">{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
--- src/section/home/inquiry.js
+++ src/section/home/inquiry.js
@@ -1,35 +1,36 @@
-import Quiry from '../../asset/quiry.jpg';
+import Quiry from "../../asset/quiry.jpg";
 import Button from "../../components/Button";
-import HorizontalLine from '../../components/HorizontalLine';
+import HorizontalLine from "../../components/HorizontalLine";
 
 export default function Inquiry() {
-
-    return(
-        <>
-        <section className="service-section md:px-20 mb-12 px-12 text-white relative w-full">
-            <img src={Quiry} alt="quiry" className='absolute'/>
-            <div className='content flex justify-center item-center'>
-                <div className='text-center absolute top-16'>
-                    <HorizontalLine />
-                    <div className='text-2xl mt-20 flex justify-center item-center gap-8 mb-8'>
-                        <div className='ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 '>
-                            <ion-icon name="logo-facebook" className=''></ion-icon>
-                        </div>
-                        <div className='ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 '>
-                            <ion-icon name="logo-twitter"></ion-icon>
-                        </div>
-                        <div className='ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 '>
-                            <ion-icon name="logo-instagram"></ion-icon>
-                        </div>
-                        <div className='ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 '>
-                            <ion-icon name="logo-youtube"></ion-icon>
-                        </div>
-                    </div>
-                    <h3 className='text-4xl font-bold text-center pb-12'>Have any project in mind?</h3>
-                    <Button name="Make Inquiry"/>
-                </div>
+  return (
+    <>
+      <section className="service-section md:px-20 mb-12 px-12 text-white relative w-full">
+        <img src={Quiry} alt="quiry" className="absolute inset-0" />
+        <div className="content flex justify-center item-center">
+          <div className="text-center absolute top-16">
+            <HorizontalLine />
+            <div className="text-2xl mt-20 flex justify-center item-center gap-8 mb-8">
+              <div className="ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 ">
+                <ion-icon name="logo-facebook" className=""></ion-icon>
+              </div>
+              <div className="ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 ">
+                <ion-icon name="logo-twitter"></ion-icon>
+              </div>
+              <div className="ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 ">
+                <ion-icon name="logo-instagram"></ion-icon>
+              </div>
+              <div className="ring-2 ring-[#66FCF1] h-12 w-12 rounded-full p-2 ">
+                <ion-icon name="logo-youtube"></ion-icon>
+              </div>
             </div>
-        </section>
-        </>
-    )
+            <h3 className="text-4xl font-bold text-center pb-12">
+              Have any project in mind?
+            </h3>
+            <Button name="Make Inquiry" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
