3e0818dacc310f030a4c9a5dd5a098a4f114958f
Rakib544 <md.rakib10122003@gmail.com>
2023-04-12 00:00:04 +0600

    some error fixed

--- src/section/home/homeFooter.js
+++ src/section/home/homeFooter.js
@@ -1,15 +1,14 @@
 // import Footer from '../../components/Layout/Footer';
 
 export default function HomeFooter() {
-
-    return(
-        <>
-        <section className="footer-section w-full ">
-            <div className='md:px-20 px-12 text-white  bg-white h-96'>
-                <p className="text-4xl">wsedrfghj</p>
-                <p className="text-4xl text-white bold">wsedrfghj</p>
-            </div>
-        </section>
-        </>
-    )
+  return (
+    <>
+      <section className="w-full ">
+        <div className="md:px-20 px-12 text-white  bg-black h-96">
+          <p className="text-4xl">wsedrfghj</p>
+          <p className="text-4xl text-white bold">wsedrfghj</p>
+        </div>
+      </section>
+    </>
+  );
 }
