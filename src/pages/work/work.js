6c0bd7156822df3cff0e2e27ed34008beb04990a
Rakib544 <md.rakib10122003@gmail.com>
2023-04-19 23:28:44 +0600

    some chages done

--- src/pages/work/work.js
+++ src/pages/work/work.js
@@ -1,12 +1,14 @@
 import Banner from "../../section/work/banner";
 import Category from "../../section/work/work";
+// import Category from "../../section/work/work";
 
 export default function Work() {
   return (
     <>
       <div>
-          <Banner />
-          <Category />
+        <Banner />
+        {/* <Category /> */}
+        <Category />
       </div>
     </>
   );
--- src/section/work/work.js
+++ src/section/work/work.js
@@ -1,45 +1,53 @@
-import useState from "react";
-import image1 from '../../asset/work_cat.jpg'
+import { useState } from "react";
+import image1 from "../../asset/work_cat.jpg";
 
-const works = [{
-    id : 1,
+const works = [
+  {
+    id: 1,
     title: "All",
     image: image1,
-    category: "Architecture"
-
-
-},
-{
-    id : 2,
+    category: "Architecture",
+  },
+  {
+    id: 2,
     title: "All",
     image: image1,
-    category: "Design"
-
-
-},
-{
-    id : 3,
+    category: "Design",
+  },
+  {
+    id: 3,
     title: "All",
     image: image1,
-    category: "Architecture"
+    category: "Architecture",
+  },
+];
 
+function Category() {
+  const [category, setCategory] = useState("");
 
-}]
-export default function Work() {
-    const[category, setCategory] = useState('');
-
-    // const selectedWorks = category === '' ? works : works.filter((work) =>{
-    //     return work.category.toLowerCase() === category.toLowerCase()
-    // })
-    // console.log(selectedWorks);
+  console.log(works);
+  console.log(category);
+  const selectedWorks =
+    category === ""
+      ? works
+      : works.filter((work) => {
+          return work.category.toLowerCase() === category.toLowerCase();
+        });
+  console.log(selectedWorks);
   return (
-    <div>
-        <div>
-            <button onClick={ () => setCategory('')}>All</button>
-            <button onClick={ () => setCategory('Architecture')}>Architecture</button>
-            <button onClick={ () => setCategory('Design')}>Design</button>
-            {/* <button>All</button> */}
+    <div className="py-32 text-white">
+      <div className="flex justify-center">
+        <div className="flex gap-4">
+          <button onClick={() => setCategory("")}>All</button>
+          <button onClick={() => setCategory("Architecture")}>
+            Architecture
+          </button>
+          <button onClick={() => setCategory("Design")}>Design</button>
+          {/* <button>All</button> */}
         </div>
+      </div>
     </div>
   );
 }
+
+export default Category;
